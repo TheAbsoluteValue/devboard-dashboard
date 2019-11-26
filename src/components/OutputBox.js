502977ca10176543b03921cf3104b53a0bf22885
dorisgjata <dorisgjata@oakland.edu>
2019-11-26 17:35:35 -0500

    Fixed styling and formatting

--- src/components/OutputBox.js
+++ src/components/OutputBox.js
@@ -1,40 +1,50 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { GridList, Paper, GridListTile, Typography } from '@material-ui/core';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { GridList, Paper, GridListTile, Typography } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
-    paper: {
-	backgroundColor: '#4d4d4d',
-	margin: theme.spacing(1),
-	color: 'white',
-	padding: theme.spacing(1),
-    },
-
-    gridList: {
-	display: 'flex',
-	flexWrap: 'wrap',
-        overflow: 'hidden',
-        flexDirection: 'column',
-	justifyContent: 'flex-start',
-	width: 500,
-	height: 450,
-    },
-}));
+  container: {
+    display: 'flex',
+    justifyContent: ' flex-end',
+    height: 'auto',
+    padding: '0.75em'
+  },
+  paper: {
+    backgroundColor: '#4d4d4d',
+    margin: theme.spacing(1),
+    color: 'white',
+    padding: theme.spacing(1)
+  },
+  gridList: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignContent: 'baseline',
+    width: 500,
+    height: '100%',
+    backgroundColor: '#fff'
+  }
+}))
 
 export default function OutputBox(props) {
-    const classes = useStyles();
-    
-    return (
-	<>
-	  <GridList className={classes.gridList} cellHeight={50} cols={1}>
-	    {props.data.map((output, i) => (
-		<GridListTile cols={1} key={i}>
-		  <Paper className={classes.paper}>
-                    <Typography component="p">{output}</Typography>
-                  </Paper>
-		</GridListTile>
-	    ))}
-	  </GridList>
-	</>
-    );
+  const classes = useStyles()
+
+  return (
+    <div className={classes.container}>
+      <GridList
+        className={classes.gridList}
+        cellHeight={60}
+        cols={1}
+        spacing={1}
+      >
+        {props.data.map((output, i) => (
+          <GridListTile cols={1} key={i}>
+            <Paper className={classes.paper}>
+              <Typography component='p'>{output}</Typography>
+            </Paper>
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  )
 }
