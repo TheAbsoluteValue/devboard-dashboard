c89d30d8b48a0bced003a07d955ca40ab3273113
kaiser <soccerbeastwc@gmail.com>
2019-11-26 21:20:49 -0500

    Fix merge conflicts 2

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ function App() {
     SerialPort.Binding = require('@serialport/binding-abstract');
     const port = new SerialPort('/dev/ttyUSB0', {baudRate: 115200});
     const parser = port.pipe(new Readline());
-
     parser.on('data', dataFrom => setData([dataFrom, ...data]));
 
     function handleClick() {
