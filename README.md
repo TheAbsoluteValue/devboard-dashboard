a47436bae4895344c5494592b4c6191835039720
kaiser <soccerbeastwc@gmail.com>
2019-11-26 21:16:40 -0500

    Fix merge conflicts

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ A little project to display messages from an ESP32 in React
 ## Install
 `npm install`
 
+`./node_modules/.bin/electron-rebuild`
+
 ## Start
 `npm start`
 
 `npm run electron`
+
--- src/components/OutputBox.js
+++ src/components/OutputBox.js
@@ -3,35 +3,35 @@ import { makeStyles } from '@material-ui/core/styles';
 import { GridList, Paper, GridListTile, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-  paper: {
-    backgroundColor: '#4d4d4d',
-    margin: theme.spacing(1),
-    color: 'white',
-    padding: theme.spacing(1)
-  },
-  gridList: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignContent: 'baseline',
-    width: 500,
-    height: '100%',
-    backgroundColor: '#fff'
-  }
+    paper: {
+	backgroundColor: '#4d4d4d',
+	margin: theme.spacing(1),
+	color: 'white',
+	padding: theme.spacing(1)
+    },
+    gridList: {
+	display: 'flex',
+	flexDirection: 'row',
+	justifyContent: 'flex-start',
+	alignContent: 'baseline',
+	width: 500,
+	height: '100%',
+	backgroundColor: '#fff'
+    }
 }));
 
 export default function OutputBox(props) {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <GridList className={classes.gridList} cellHeight={60} cols={1} spacing={1}>
-      {props.data.map((output, i) => (
-        <GridListTile cols={1} key={i}>
-          <Paper className={classes.paper}>
-            <Typography component='p'>{output}</Typography>
-          </Paper>
-        </GridListTile>
-      ))}
-    </GridList>
-  );
-}
+    return (
+	<GridList className={classes.gridList} cellHeight={60} cols={1} spacing={1}>
+	  {props.data.map((output, i) => (
+	      <GridListTile cols={1} key={i}>
+		<Paper className={classes.paper}>
+		  <Typography component='p'>{output}</Typography>
+		</Paper>
+	      </GridListTile>
+          ))}
+        </GridList>
+    );
+
